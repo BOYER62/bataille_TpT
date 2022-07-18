d8401f74ba221e4d335d9e594f471a3416c2970c
BOYER62 <boyer.olivier2@free.fr>
2022-07-18 16:16:32 +0200

    V 0.7.18.1

--- public_html/asset/scrypt.js
+++ public_html/asset/scrypt.js
@@ -2,46 +2,57 @@ const player = document.getElementById('player');
 const optionHero = document.getElementById('selectHeros');
 const nameImg = document.getElementById("nameImg");
 const requestURLHeros = './asset/json/heros.json';
-const requestHeros = new XMLHttpRequest();
 const requestURLPlayer = './asset/json/player.json';
 const requestPlayer = new XMLHttpRequest();
 const requestURLVehicule = './asset/json/vehicule.json';
 const requestVehicule = new XMLHttpRequest();
 
-requestHeros.open('GET', requestURLHeros);
-requestHeros.responseType = 'json';
-requestHeros.send();
+
+optionHero.addEventListener('change', function()
+{
+    const requestHeros = new XMLHttpRequest();
+    requestHeros.open('GET', requestURLHeros);
+    requestHeros.responseType = 'json';
+    requestHeros.send();
+    requestHeros.onload = function(){
+    const jsonHero = requestHeros.response;
+    nameImg.src=`../images/Chara/${jsonHero[index]['img']}`;
+    }
+    const index = optionHero.selectedIndex; 
+})
+
+
 requestPlayer.open('GET', requestURLPlayer);
 requestPlayer.responseType = 'json';
 requestPlayer.send();
 requestVehicule.open('GET', requestURLVehicule);
 requestVehicule.responseType = 'json';
 requestVehicule.send();
-requestHeros.onload = function(){
-    const jsonHero = requestHeros.response;
-    console.log(jsonHero);
-}
-requestPlayer.onload = function(){
-    const jsonPlayer = requestPlayer.response;
-    console.log(jsonPlayer);
-}
-requestVehicule.onload = function(){
-    const jsonVehicule = requestVehicule.response;
-    console.log(jsonVehicule);
-}
 
-// recupaire le nom du hero dans la liste deroulante
-optionHero.addEventListener('change', function()
-{
+// optionHero.addEventListener('change', function()
+// {
+//     requestHeros.onload = function(){
+//         const jsonHero = requestHeros.response;
+//         console.log(jsonHero);
+//         switch(jsonHero){
+//             case jsonHero[0]['name_hero'] :
+//                 nameImg.src=`../images/Chara/${jsonHero[0]['img']}`;
+//                 console.log(optionHero.options[optionHero.selectedIndex].text);    
+//         }
+// }
+// requestPlayer.onload = function(){
+//     const jsonPlayer = requestPlayer.response;
+//     console.log(jsonPlayer);
+// }
+// requestVehicule.onload = function(){
+//     const jsonVehicule = requestVehicule.response;
+//     console.log(jsonVehicule);
+// }
+
+// // recupaire le nom du hero dans la liste deroulante
   
-    // switch(jsonHero){
-    //     case jsonHero[0]['name_hero'] :
-    //         nameImg.src=`../images/Chara/${jsonHero[0]['img']}`;
-    //         console.log(optionHero.options[optionHero.selectedIndex].text);    
-    // }
-    console.log('test2');
-    console.log(jsonPlayer);
-});
+//     //console.log(jsonHero[0]["name_hero"]);
+// });
 
 
 
